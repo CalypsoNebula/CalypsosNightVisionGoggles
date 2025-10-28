a413383bc51450d32eba6961130c0c62a7ff35ad
SettingDust <settingdust@gmail.com>
2025-10-28 10:42:16 +0800

    feat: add armor for variants

--- src/common/1.20.1/main/kotlin/settingdust/calypsos_nightvision_goggles/v1_20/item/nightvision_goggles/NightvisionGogglesItem.kt
+++ src/common/1.20.1/main/kotlin/settingdust/calypsos_nightvision_goggles/v1_20/item/nightvision_goggles/NightvisionGogglesItem.kt
@@ -1,13 +1,25 @@
 package settingdust.calypsos_nightvision_goggles.v1_20.item.nightvision_goggles
 
+import com.google.common.collect.ImmutableMultimap
+import com.google.common.collect.Multimap
 import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.ai.attributes.Attribute
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
+import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.NightvisionGogglesItem
 import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.NightvisionGogglesVariant
+import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.variant.RegularVariant
+import java.util.*
 
 open class NightvisionGogglesItem(variant: NightvisionGogglesVariant) : NightvisionGogglesItem(variant) {
+    companion object {
+        val ARMOR_ID = UUID.fromString("c4255a94-b552-4e33-8dc9-90290efdb544")
+    }
+
     class Factory : NightvisionGogglesItem.Factory {
         override fun invoke(variant: NightvisionGogglesVariant) =
             settingdust.calypsos_nightvision_goggles.v1_20.item.nightvision_goggles.NightvisionGogglesItem(variant)
@@ -20,5 +32,19 @@ open class NightvisionGogglesItem(variant: NightvisionGogglesVariant) : Nightvis
         isAdvanced: TooltipFlag
     ) {
         components.appendTooltip(stack)
+    }
+
+    override fun getDefaultAttributeModifiers(slot: EquipmentSlot): Multimap<Attribute, AttributeModifier> {
+        if (variant == RegularVariant) return ImmutableMultimap.of()
+        val builder = ImmutableMultimap.builder<Attribute, AttributeModifier>()
+        when (slot) {
+            EquipmentSlot.HEAD -> builder.put(
+                Attributes.ARMOR,
+                AttributeModifier(ARMOR_ID, "Nightvision Goggles", 2.0, AttributeModifier.Operation.ADDITION)
+            )
+
+            else -> {}
+        }
+        return builder.build()
     }
 }
--- src/common/1.21.1/main/kotlin/settingdust/calypsos_nightvision_goggles/v1_21/item/nightvision_goggles/NightvisionGogglesItem.kt
+++ src/common/1.21.1/main/kotlin/settingdust/calypsos_nightvision_goggles/v1_21/item/nightvision_goggles/NightvisionGogglesItem.kt
@@ -1,10 +1,16 @@
 package settingdust.calypsos_nightvision_goggles.v1_21.item.nightvision_goggles
 
 import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.EquipmentSlotGroup
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
+import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
+import net.minecraft.world.item.component.ItemAttributeModifiers
+import settingdust.calypsos_nightvision_goggles.CalypsosNightVisionGogglesKeys
 import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.NightvisionGogglesItem
 import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.NightvisionGogglesVariant
+import settingdust.calypsos_nightvision_goggles.item.nightvision_goggles.variant.RegularVariant
 
 class NightvisionGogglesItem(variant: NightvisionGogglesVariant) : NightvisionGogglesItem(variant) {
 
@@ -20,5 +26,20 @@ class NightvisionGogglesItem(variant: NightvisionGogglesVariant) : NightvisionGo
         isAdvanced: TooltipFlag
     ) {
         components.appendTooltip(stack)
+    }
+
+    override fun getDefaultAttributeModifiers(): ItemAttributeModifiers {
+        if (variant == RegularVariant) return ItemAttributeModifiers.EMPTY
+        val builder = ItemAttributeModifiers.builder()
+        builder.add(
+            Attributes.ARMOR,
+            AttributeModifier(
+                CalypsosNightVisionGogglesKeys.NightvisionGoggles,
+                2.0,
+                AttributeModifier.Operation.ADD_VALUE
+            ),
+            EquipmentSlotGroup.HEAD
+        )
+        return builder.build()
     }
 }
